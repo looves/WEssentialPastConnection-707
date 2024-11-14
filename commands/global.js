eeeedf1ea3e4be721a4fc5034402d2a9945a99d4
looves <bryxnjtb@gmail.com>
2024-11-14 00:06:23 -0500

    Update global.js

--- commands/global.js
+++ commands/global.js
@@ -27,147 +27,149 @@ module.exports = {
         .setDescription('Rareza de la carta')
         .setRequired(false)),
 
-async execute(interaction) {
-  const idolFilter = interaction.options.getString('idol');
-  const grupoFilter = interaction.options.getString('grupo');
-  const eraFilter = interaction.options.getString('era');
-  const eshortFilter = interaction.options.getString('eshort');
-  const rarity = interaction.options.getString('rarity');
-
-  try {
- 
-    try {
-      await interaction.deferReply();
-    } catch (error) {
-      // Si hay un error al deferir la respuesta, simplemente hacer un return para evitar que el bot se caiga
-      console.error('Error al deferir la respuesta:', error);
-      return; // Termina la ejecución del comando si ocurre un error
-    }// Defer the reply immediately
-
-    const allDroppedCards = await DroppedCard.find();
-    let filteredCards = allDroppedCards;
-
-    const cleanString = (str) => {
-      return String(str).replace(/[^\w\s]/g, '').toLowerCase(); // Limpiar caracteres especiales
-    };
-
-    if (idolFilter) {
-      filteredCards = filteredCards.filter(card => card.idol && card.idol.toLowerCase().includes(idolFilter.toLowerCase()));
-    }
-    if (grupoFilter) {
-      filteredCards = filteredCards.filter(card => card.grupo && cleanString(card.grupo).includes(cleanString(grupoFilter)));
-    }    
-    if (eraFilter) {
-      filteredCards = filteredCards.filter(card => card.era && cleanString(card.era).includes(cleanString(eraFilter)));
-    }
-    if (eshortFilter) {
-      filteredCards = filteredCards.filter(card => card.eshort && card.eshort.toLowerCase().includes(eshortFilter.toLowerCase()));
-    }
-    if (rarity) {
-      filteredCards = filteredCards.filter(card => card.rarity === rarity);
-    }
-
+  async execute(interaction) {
+    const idolFilter = interaction.options.getString('idol');
+    const grupoFilter = interaction.options.getString('grupo');
+    const eraFilter = interaction.options.getString('era');
+    const eshortFilter = interaction.options.getString('eshort');
+    const rarity = interaction.options.getString('rarity');
     const maxFields = 9;
-    const totalPages = Math.ceil(filteredCards.length / maxFields);
-    let currentPage = 0;
 
-    if (filteredCards.length === 0) {
-      return interaction.editReply({ content: 'No se encontraron cartas con los criterios especificados.', ephemeral: true });
-    }
+    try {
+      await interaction.deferReply();  // Deferir la respuesta inmediatamente
 
-    const createEmbed = (page) => {
-      const embed = new EmbedBuilder()
-        .setAuthor({ name: `${filteredCards.length} cartas en total`, iconURL: interaction.user.displayAvatarURL() })
-        .setTimestamp()
-        .setColor('#60a5fa')
-        .setFooter({ text: `Página ${page + 1} de ${totalPages}` });
+      // Preparar el filtro de la consulta
+      let filter = {};
 
-      const startIndex = page * maxFields;
-      const endIndex = Math.min(startIndex + maxFields, filteredCards.length);
-      for (let i = startIndex; i < endIndex; i++) {
-        const card = filteredCards[i];
-        embed.addFields({
-          name: `${card.idol} <:dot:1296707029087555604> \`#${card.copyNumber}\``,
-          value: `${rarityToEmojis(card.rarity)} ${card.grupo} ${card.eshort}\n\`\`\`${card.uniqueCode}\`\`\` <@${card.userId}>`,
-          inline: true,
-        });
+      if (idolFilter) {
+        filter.idol = { $regex: idolFilter, $options: 'i' };  // Filtrar por idol de manera insensible a mayúsculas/minúsculas
       }
-      return embed;
-    };
-
-    const getButtonRow = (currentPage, totalPages) => {
-      const row = new ActionRowBuilder()
-        .addComponents(
-          new ButtonBuilder()
-            .setCustomId('first')
-            .setEmoji("<:first:1290467842462060605>")
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage === 0),
-          new ButtonBuilder()
-            .setCustomId('previous')
-            .setEmoji("<:prev:1290467827739787375>")
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage === 0),
-          new ButtonBuilder()
-            .setCustomId('close')
-            .setEmoji("<:close:1290467856437481574>")
-            .setStyle(ButtonStyle.Danger),
-          new ButtonBuilder()
-            .setCustomId('next')
-            .setEmoji("<:next:1290467800065769566>")
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage >= totalPages - 1),
-          new ButtonBuilder()
-            .setCustomId('last')
-            .setEmoji("<:last:1290467815127519322>")
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage >= totalPages - 1)
-        );
-
-      return row;
-    };
-
-    // Muestra la respuesta inicial con los botones
-    const message = await interaction.editReply({
-      embeds: [createEmbed(currentPage)],
-      components: [getButtonRow(currentPage, totalPages)],
-    });
-
-    // Configura el colector de botones
-    const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60000 });
-
-    collector.on('collect', async i => {
-      if (i.user.id !== interaction.user.id) {
-        return i.reply({ content: 'No puedes interactuar con este botón.', ephemeral: true });
+      if (grupoFilter) {
+        filter.grupo = { $regex: grupoFilter.replace(/[^\w\s]/g, ''), $options: 'i' };  // Filtrar por grupo, limpiando caracteres especiales
+      }
+      if (eraFilter) {
+        filter.era = { $regex: eraFilter.replace(/[^\w\s]/g, ''), $options: 'i' };  // Filtrar por era
+      }
+      if (eshortFilter) {
+        filter.eshort = { $regex: eshortFilter, $options: 'i' };  // Filtrar por era corta
+      }
+      if (rarity) {
+        filter.rarity = rarity;  // Filtrar por rareza
       }
 
-      if (i.customId === 'previous' && currentPage > 0) {
-        currentPage--;
-      } else if (i.customId === 'next' && currentPage < totalPages - 1) {
-        currentPage++;
-      } else if (i.customId === 'first') {
-        currentPage = 0;
-      } else if (i.customId === 'last') {
-        currentPage = totalPages - 1;
-      } else if (i.customId === 'close') {
-        await i.update({ content: '**/global cerrado...**', embeds: [], components: [] });
-        return collector.stop();
+      // Consultar las cartas que coinciden con el filtro
+      const allFilteredCards = await DroppedCard.find(filter);
+
+      if (allFilteredCards.length === 0) {
+        return interaction.editReply({ content: 'No se encontraron cartas con los criterios especificados.', ephemeral: true });
       }
 
-      // Aquí se usa i.update() correctamente
-      await i.update({
+      // Paginación
+      const totalPages = Math.ceil(allFilteredCards.length / maxFields);
+      let currentPage = 0;
+
+      const createEmbed = (page) => {
+        const embed = new EmbedBuilder()
+          .setAuthor({ name: `${allFilteredCards.length} cartas encontradas`, iconURL: interaction.user.displayAvatarURL() })
+          .setTimestamp()
+          .setColor('#60a5fa')
+          .setFooter({ text: `Página ${page + 1} de ${totalPages}` });
+
+        const startIndex = page * maxFields;
+        const endIndex = Math.min(startIndex + maxFields, allFilteredCards.length);
+
+        // Añadir campos a cada página
+        for (let i = startIndex; i < endIndex; i++) {
+          const card = allFilteredCards[i];
+          embed.addFields({
+            name: `${card.idol} <:dot:1296707029087555604> \`#${card.copyNumber}\``,
+            value: `${rarityToEmojis(card.rarity)} ${card.grupo} ${card.eshort}\n\`\`\`${card.uniqueCode}\`\`\` <@${card.userId}>`,
+            inline: true,
+          });
+        }
+
+        return embed;
+      };
+
+      const getButtonRow = (currentPage, totalPages) => {
+        const row = new ActionRowBuilder()
+          .addComponents(
+            new ButtonBuilder()
+              .setCustomId('first')
+              .setEmoji("<:first:1290467842462060605>")
+              .setStyle(ButtonStyle.Secondary)
+              .setDisabled(currentPage === 0),
+            new ButtonBuilder()
+              .setCustomId('previous')
+              .setEmoji("<:prev:1290467827739787375>")
+              .setStyle(ButtonStyle.Secondary)
+              .setDisabled(currentPage === 0),
+            new ButtonBuilder()
+              .setCustomId('close')
+              .setEmoji("<:close:1290467856437481574>")
+              .setStyle(ButtonStyle.Danger),
+            new ButtonBuilder()
+              .setCustomId('next')
+              .setEmoji("<:next:1290467800065769566>")
+              .setStyle(ButtonStyle.Secondary)
+              .setDisabled(currentPage >= totalPages - 1),
+            new ButtonBuilder()
+              .setCustomId('last')
+              .setEmoji("<:last:1290467815127519322>")
+              .setStyle(ButtonStyle.Secondary)
+              .setDisabled(currentPage >= totalPages - 1)
+          );
+
+        return row;
+      };
+
+      // Muestra la respuesta inicial con los botones
+      const message = await interaction.editReply({
         embeds: [createEmbed(currentPage)],
         components: [getButtonRow(currentPage, totalPages)],
       });
-    });
 
-    collector.on('end', async () => {
-      await message.edit({ components: [] }); // Se eliminan los botones después de que termine la interacción
-    });
+      // Configura el colector de botones
+      const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60000 });
 
-  } catch (error) {
-    console.error('Error al ejecutar el comando /global:', error);
-    await interaction.editReply({ content: 'Ocurrió un error al buscar las cartas. Por favor, inténtalo de nuevo.', ephemeral: true });
+      collector.on('collect', async i => {
+        // Verifica que la interacción pertenezca al mensaje correcto
+        if (i.message.id !== message.id) return;
+
+        // Verifica que el usuario es el que ejecutó el comando
+        if (i.user.id !== interaction.user.id) {
+          return i.reply({ content: 'No puedes interactuar con este botón.', ephemeral: true });
+        }
+
+        if (i.customId === 'previous' && currentPage > 0) {
+          currentPage--;
+        } else if (i.customId === 'next' && currentPage < totalPages - 1) {
+          currentPage++;
+        } else if (i.customId === 'first') {
+          currentPage = 0;
+        } else if (i.customId === 'last') {
+          currentPage = totalPages - 1;
+        } else if (i.customId === 'close') {
+          await i.update({ content: '**/global cerrado...**', embeds: [], components: [] });
+          collector.stop();  // Detener el colector después de cerrar
+          return;
+        }
+
+        // Actualiza el mensaje con la nueva página
+        await i.update({
+          embeds: [createEmbed(currentPage)],
+          components: [getButtonRow(currentPage, totalPages)],
+        });
+      });
+
+      collector.on('end', async () => {
+        // Deshabilita los botones después de que termine el tiempo
+        await message.edit({ components: [] });
+      });
+
+    } catch (error) {
+      console.error('Error al ejecutar el comando /global:', error);
+      await interaction.editReply({ content: 'Hubo un error al procesar el comando. Por favor, inténtalo de nuevo.', ephemeral: true });
+    }
   }
-}
 };
