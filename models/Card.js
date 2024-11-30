9d204df78a5707c8eda86b42320573371c885fa3
looves <bryxnjtb@gmail.com>
2024-11-30 16:52:35 -0500

    Update Card.js

--- models/Card.js
+++ models/Card.js
@@ -1,13 +1,13 @@
 const { Schema, model } = require('mongoose');
 
 const cardSchema = new Schema({
-  idol: { type: String, required: true },
-  grupo: { type: String, required: true },
-  era: { type: String, required: true },
-  eshort: { type: String, required: true},
-  rarity: { type: String, required: true }, // Agregado
-  image: { type: String, required: true },
-  event: { type: String, default: false },
+  idol: { type: String, required: true, index: true },
+  grupo: { type: String, required: true, index: true },
+  era: { type: String, required: true, index: true },
+  eshort: { type: String, required: true, index: true },
+  rarity: { type: String, required: true, index: true }, // Agregado
+  image: { type: String, required: true, index: true },
+  event: { type: String, default: false, index: true },
   count: { type: Number, default: 0 }, // Para llevar el conteo de apariciones
 });
 
