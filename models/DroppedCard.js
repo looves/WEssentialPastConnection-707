f2f50ed375a305177ffec62ef0f77b56fbe2ac7e
looves <bryxnjtb@gmail.com>
2024-11-30 16:52:03 -0500

    Update DroppedCard.js

--- models/DroppedCard.js
+++ models/DroppedCard.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const droppedCardSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // ID del usuario que obtuvo la carta
-  cardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Card', required: true }, // Referencia a la carta obtenida
-  idol: { type: String, required: true },
-  grupo: { type: String, required: true },
-  era: { type: String, required: true },
-  eshort: { type: String, required: true },
-  rarity: { type: String, required: true },
-  uniqueCode: { type: String, required: true },
-  copyNumber: { type: Number, required: true },
+  userId: { type: String, required: true, index: true }, // ID del usuario que obtuvo la carta
+  cardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Card', required: true, index: true }, // Referencia a la carta obtenida
+  idol: { type: String, required: true, index: true },
+  grupo: { type: String, required: true, index: true },
+  era: { type: String, required: true, index: true },
+  eshort: { type: String, required: true, index: true },
+  rarity: { type: String, required: true, index: true },
+  uniqueCode: { type: String, required: true, index: true },
+  copyNumber: { type: Number, required: true, index: true },
   command: { type: String, required: true }, // Comando en el que se dropeó la carta
 });
 
