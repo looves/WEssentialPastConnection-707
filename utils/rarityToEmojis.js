43bccdaff05e2161ddc5fb0aa07029ce2f844a86
looves <bryxnjtb@gmail.com>
2024-12-25 23:16:34 -0500

    Update rarityToEmojis.js

--- utils/rarityToEmojis.js
+++ utils/rarityToEmojis.js
@@ -1,10 +1,16 @@
 // ../utils/rarityToEmojis.js
-module.exports = (rarity) => {
+module.exports = (rarityOrEvent) => {
     const emojis = {
         1: '<:stars:1296707011500838932>',
         2: '<:stars:1296707011500838932><:stars:1296707011500838932>',
-        3: '<:stars:1296707011500838932><:stars:1296707011500838932><:stars:1296707011500838932>'
+        3: '<:stars:1296707011500838932><:stars:1296707011500838932><:stars:1296707011500838932>',
+
+        XMAS: '<:xmas:1321692124554723389>'
     };
 
-    return emojis[rarity] || '<:stars:1296707011500838932>'; // Default a una estrella si la rareza es inválida
+    if (typeof rarityOrEvent === 'string' && emojis[rarityOrEvent]) {
+        return emojis[rarityOrEvent];
+    }
+
+    return emojis[rarityOrEvent] || '<:stars:1296707011500838932>'; // Default a una estrella si la rareza es inválida
 };
