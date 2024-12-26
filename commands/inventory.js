f734e5cdc6905ea8fd0d2bed25937c0656d05ff0
looves <bryxnjtb@gmail.com>
2024-12-26 01:46:42 -0500

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, Butt
 const DroppedCard = require('../models/DroppedCard');
 const rarityToEmojis = require('../utils/rarityToEmojis');
 
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('inventory')
@@ -37,8 +38,10 @@ module.exports = {
     const eraFilter = interaction.options.getString('era');
     const eshortFilter = interaction.options.getString('eshort');
     const rarityFilter = interaction.options.getString('rarity');
-    const eventFilter = interaction.options.getString('event'); 
+    const eventFilter = interaction.options.getString('event'); // Filtro para el evento
 
+    const user = await interaction.client.users.fetch(userId);
+    
     try {
       await interaction.deferReply();
 
@@ -54,12 +57,12 @@ module.exports = {
       if (eraFilter) query.era = new RegExp(escapeRegex(eraFilter), 'i');
       if (eshortFilter) query.eshort = new RegExp(escapeRegex(eshortFilter), 'i');
       if (rarityFilter) query.rarity = new RegExp(escapeRegex(rarityFilter), 'i');
-      if (eventFilter) query.event = new RegExp(escapeRegex(eventFilter), 'i');
+      if (eventFilter) query.event = new RegExp(escapeRegex(eventFilter), 'i'); // Filtro por evento
 
       // Contar el número total de cartas que coinciden con los filtros
       const totalCards = await DroppedCard.countDocuments(query);
       if (totalCards === 0) {
-        return interaction.editReply({ content: 'No tienes cartas en tu inventario que coincidan con los filtros especificados.', ephemeral: true });
+        return interaction.editReply({ content: 'No tienes cartas en tu inventario que coincidan con los filtros especificados.'});
       }
 
       const maxFields = 9;  // Mostrar 9 cartas por página
@@ -69,7 +72,7 @@ module.exports = {
       // Función para crear el embed de la página actual
       const createEmbed = async (page) => {
         const cards = await DroppedCard.find(query)  // Buscar cartas con los filtros aplicados
-          .select('idol eshort grupo copyNumber rarity uniqueCode event')  // Seleccionar solo los campos necesarios
+          .select('idol eshort grupo copyNumber rarity uniqueCode event')  // Seleccionar solo los campos necesarios, incluyendo event
           .skip(page * maxFields)  // Omitir los resultados previos
           .limit(maxFields)  // Limitar a 9 resultados
           .lean();  // Usar lean para mejorar el rendimiento
@@ -80,8 +83,7 @@ module.exports = {
           .setColor('#60a5fa')
           .setFooter({ text: `Página ${page + 1} de ${totalPages}` });
 
-        cards.forEach(card => {
-
+      cards.forEach(card => {
           let emoji = rarityToEmojis(card.rarity); // Emoji de rareza por defecto
 
           // Si hay un evento, usamos el emoji del evento
@@ -98,7 +100,6 @@ module.exports = {
 
         return embed;
       };
-
       // Crear los botones de navegación
       const getButtonRow = (currentPage, totalPages) => {
         return new ActionRowBuilder()
@@ -142,6 +143,7 @@ module.exports = {
       collector.on('collect', async i => {
         // Verificar que la interacción pertenece al mensaje correcto
         if (i.message.id !== message.id) return;
+
         // Verificar que el usuario es el que ejecutó el comando
         if (i.user.id !== interaction.user.id) {
           return i.reply({ content: 'No puedes interactuar con este botón.', ephemeral: true });
@@ -156,7 +158,8 @@ module.exports = {
         } else if (i.customId === 'last') {
           currentPage = totalPages - 1;
         } else if (i.customId === 'close') {
-          await i.update({ content: `**</inventory:1291579000044650513> cerrado...**`, embeds: [], components: [] });
+          // Utiliza i.update para cerrar el inventario
+          await i.update({ content: `**/inventory cerrado...**`, embeds: [], components: [] });
           collector.stop();  // Detener el colector después de cerrar
           return;
         }
@@ -168,6 +171,7 @@ module.exports = {
         });
       });
 
+
       collector.on('end', async () => {
         try {
           // Si el mensaje sigue estando accesible, usa `message.edit()` para deshabilitar los botones
@@ -178,6 +182,7 @@ module.exports = {
           console.error("Error al actualizar los componentes:", error);
         }
       });
+
 
     } catch (error) {
       console.error('Error al ejecutar el comando /inventory:', error);
