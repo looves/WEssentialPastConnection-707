63e97c34164ca32d9e60ea8b376b83553612ee27
looves <bryxnjtb@gmail.com>
2025-01-09 23:24:04 -0500

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.write("Wonho is ready!"); // Escribiendo en la respuesta
-    res.end(' responding to commands...'); // Enviando la respuesta y finalizando
-}).listen(8080);
